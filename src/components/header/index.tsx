99e1a08822d653a2220a074043efde0ad5b98189
Denis Moreira <silvad281@gmail.com>
2023-07-10 14:04:52 -0300

    feat: criando header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,33 @@
+import LogoImg from "../../assets/logo.svg";
+import { Link } from "react-router-dom";
+import { FiUser, FiLogIn } from "react-icons/fi";
+
 export function Header() {
+   const signer = false;
+   const loadingAuth = false;
+
    return (
-      <div>
-         <h1>componente - Header</h1>
+      <div className="w-full flex items-center justify-center h-16 bg-white drop-shadow mb-4">
+         <header className="flex w-full max-w-7xl px-4 mx-auto items-center justify-between">
+            <Link to="/">
+               <img src={LogoImg} alt="logo webcarros" />
+            </Link>
+
+            {!loadingAuth && signer && (
+               <Link to="/dashboard">
+                  <div className="border-2 rounded-full p-1 border-gray-900">
+                     <FiUser size={22} color="#000" />
+                  </div>
+               </Link>
+            )}
+            {!loadingAuth && !signer && (
+               <Link to="/login">
+                  <div className="border-2 rounded-full p-1 border-gray-900">
+                     <FiLogIn size={22} color="#000" />
+                  </div>
+               </Link>
+            )}
+         </header>
       </div>
    );
 }
